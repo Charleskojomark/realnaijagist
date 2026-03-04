f8b6df382855d61b199c53c13c8d38b360d6374d
Charleskojomark <markcharleskojo@.com>
2026-03-04 12:14:37 +0100

    Touch wsgi.py to force Namecheap Passenger restart and flush template cache

--- realnaijagist/wsgi.py
+++ realnaijagist/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'realnaijagist.settings')
 
 application = get_wsgi_application()
+
+# DEPLOY_RESTART_TRIGGER: 2026-03-04T12:10
