372f9117bfa809feac483926313ab2ea0f80b0d6
Charleskojomark <markcharleskojo@.com>
2026-03-05 09:38:06 +0100

    fix(ui): resolve template tag syntax error in related posts section

--- realnaijagist/wsgi.py
+++ realnaijagist/wsgi.py
@@ -25,3 +25,4 @@ application = get_wsgi_application()
 # Final search and carousel fix: Thu Mar  5 09:17:13 AM WAT 2026
 # Views fix: Thu Mar  5 09:23:24 AM WAT 2026
 # Share UI Push: Thu Mar  5 09:33:57 AM WAT 2026
+# Template Tags Fix: Thu Mar  5 09:38:06 AM WAT 2026
